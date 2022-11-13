48e62c12e01917096f5e7453980e572dc3134a37
Memory Clutter <memclutter@gmail.com>
2022-11-13 13:00:41 +0300

    Stage 0. Created django models index and migration

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    class Meta:
+        indexes = (
+            models.Index(fields=['-pub_date']),
+        )
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
